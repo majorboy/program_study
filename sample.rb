e107e00cca5101f67187d5cb56aeabf582c53a84
Kengo Nagata <majorboy47@gmail.com>
2019-04-09 21:29:03 +0900

    add new file

---
+++ sample.rb
@@ -0,0 +1,64 @@
+# for i in [1,2,3] do
+#   p i
+       
+# end
+
+# 1.upto(10) do |i|
+#   p i   
+# end
+
+# 10.downto(1) do |i|
+#   p i
+# end
+
+# puts 1.class 
+  
+# puts "Hello world".class
+
+# puts 20.1.class
+
+# puts nil.class
+
+# puts [].class
+# true.class
+# String.class
+
+# animals = ['dog' , 'cat' , 'mouse']
+
+# p animals
+# p animals[0]
+# animals << 'bird'
+# p animals
+# animals[2] = 'human'
+# p animals
+# animals.delete('cat')
+# p animals
+# animals.insert(2,'yamada')
+# p animals
+
+# man = { 'height' => 170, 'weight' => 65 }
+# p man
+# p man['height']
+# p man['weight']
+
+# woman = { :height => 160, :weight => 50 }
+# p woman
+
+# child = { height: 110, weight: 20 }
+# p child
+
+# child[:age] = 7 
+# p child
+
+# child.delete(:age)
+# p child
+
+# p child[:age]
+
+# child.store(:age, 7)
+# p child
+
+animals = %w(dog cat mouse)
+p animals
+animals = %i(dog cat mouse)
+p animals
